e29b47537adaf98ca76e279188fdd8bcbb71f611
Aayush Kapoor <xeoneux@icloud.com>
2016-05-19 00:25:02 +0530

    Display Photos

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 const Photo = React.createClass({
   render() {
+    const {post, index, comments} = this.props;
     return (
       <figure className="grid-figure">
-        I'm Photo
+        <div className="grid-photo-wrap">
+          <Link to={`/view/${post.code}`}>
+            <img src={post.display_src} alt={post.caption} className="grid-photo"/>
+          </Link>
+        </div>
       </figure>
     );
   }
