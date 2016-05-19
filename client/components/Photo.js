80875fe1e6eba7eb289ac53fcfb8d310f9472831
Aayush Kapoor <xeoneux@icloud.com>
2016-05-19 13:32:47 +0530

    Add styles and buttons

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router';
+import CSSTransitionGroup from 'react-addons-css-transition-group';
 
 const Photo = React.createClass({
   render() {
@@ -10,7 +11,31 @@ const Photo = React.createClass({
           <Link to={`/view/${post.code}`}>
             <img src={post.display_src} alt={post.caption} className="grid-photo"/>
           </Link>
+
+          <CSSTransitionGroup
+            transitionName="like"
+            transitionEnterTimeout={500}
+            transitionLeaveTimeout={500}>
+            <span key={post.likes} className="likes-heart">
+              {post.likes}
+            </span>
+          </CSSTransitionGroup>
         </div>
+
+        <figcaption>
+          <p>{post.caption}</p>
+          <div className="control-buttons">
+            <button className="likes">&hearts; {post.likes}</button>
+
+            <Link className="button" to={`/view/${post.code}`}>
+              <span className="comment-count">
+                <span className="speech-bubble"></span>
+
+                {comments[post.code] ? comments[post.code].length : 0}
+              </span>
+            </Link>
+          </div>
+        </figcaption>
       </figure>
     );
   }
