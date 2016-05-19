f45bf2cb533a8775484ae7c3337696ce0834f9f3
Aayush Kapoor <xeoneux@icloud.com>
2016-05-19 14:12:45 +0530

    Display single photo

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
+import Photo from './Photo';
+
 const Single = React.createClass({
   render() {
+    const index = this.props.posts.findIndex((post) => {
+      return post.code === this.props.params.postId;
+    });
+    const post = this.props.posts[index];
+
     return (
       <div className="single-photo">
-        I'm Single
+        <Photo index={index} post={post} {...this.props}></Photo>
       </div>
     );
   }
