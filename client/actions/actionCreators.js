22f6a3650794341b9cfc3285825bfd7714037f93
Aayush Kapoor <xeoneux@icloud.com>
2016-05-19 13:56:30 +0530

    Export action creators

--- client/actions/actionCreators.js
+++ client/actions/actionCreators.js
@@ -1,11 +1,11 @@
-function increment(index) {
+export function increment(index) {
   return {
     type: 'INCREMENT_LIKES',
     index
   }
 }
 
-function addComment(postId, author, comment) {
+export function addComment(postId, author, comment) {
   return {
     type: 'ADD_COMMENT',
     postId,
@@ -14,7 +14,7 @@ function addComment(postId, author, comment) {
   }
 }
 
-function removeComment(index, postId) {
+export function removeComment(index, postId) {
   return {
     type: 'REMOVE_COMMENT',
     index,
